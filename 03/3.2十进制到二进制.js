522ee33c488c28bfffa60c98b461d23c1509e1e0
吕其鑫 <BetterLvqx@163.com>
2017-05-09 10:08:49 +0800

    栈的在进制中的应用

--- 03/3.2十进制到二进制.js
+++ 03/3.2十进制到二进制.js
@@ -58,3 +58,21 @@ function divideBy2(decNumber) {
 }
 
 // divideBy2(10);
+ 
+ function baseConverter(decNumber, base) {
+     var remStack = new Stack(),
+         rem,
+         baseString = '',
+         digits = '0123456789ABCDEF';
+     while (decNumber > 0) {
+         rem = Math.floor(decNumber % base);
+         remStack.push(rem);
+         decNumber = Math.floor(decNumber / base);
+     }
+
+     while(!remStack.isEmpty()) {
+         baseString += digits[remStack.pop()];
+     }
+     return baseString;
+ }
+ console.log(baseConverter(100345, 2))
